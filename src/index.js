925fff7e551770a6339af1e73383e6fc11326cbb
RomanGorenkov <roman.gorenkov.one@gmail.com>
2019-02-25 19:30:28 +0300

    love-triangle: complete

--- src/index.js
+++ src/index.js
@@ -4,4 +4,11 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var count = 0;
+  for (var i = 0; i < preferences.length; i++) {
+  	if (i + 1 == preferences[preferences[preferences[i] - 1] - 1] && preferences[preferences[i] - 1] > i && preferences[i] - 1 > i) {
+  		count++;
+  	}
+  }
+  return count;
 };
